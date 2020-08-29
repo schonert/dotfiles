13c16557984411565f5c345c8596d6b69b06d408
Stefan <sh@schonert.dk>
2020-08-29 21:41:30 +0200

    Rewrite setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Simple setup for new machines - comes with a default Frontend/Design stack
 
 # Git
 GIT_EMAIL="sh@schonert.dk"
@@ -9,84 +8,131 @@ GIT_NAME="Stefan Schonert"
 SSH_EXT=".pub"
 SSH_FILE=~/.ssh/id_rsa
 SSH_PUBLIC=$SSH_FILE$SSH_EXT
+SSH_CONFIG_FILE=~/.ssh/config
 
 # Symbolic files
 SYM_FILES=( .vimrc .bashrc .zshrc .tmux.conf .gitignore Brewfile )
 
-# Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+function main() {
 
-# Run brewfile
-brew tap Homebrew/bundle
-brew bundle
+	# Dotfiles
+	create_symlinks
 
-yarn global add xbuild fkill-cli
+	# Install homebrew
+	if ! command -v brew &> /dev/null; then
+		install_brew
+	fi
 
-# GIT!
-echo ""
-echo "Fancy git colors"
-git config --global color.ui true
-echo ""
+	install_programs
 
-echo "git email $GIT_EMAIL"
-git config --global user.email $GIT_EMAIL
-echo ""
+	setup_git
 
-echo "git name $GIT_NAME"
-git config --global user.name $GIT_NAME
-echo ""
+	# SSH
+	if ! test -f $SSH_PUBLIC; then
+		setup_ssh
+	fi
 
-# SSH!
-echo ""
-echo "New ssh key coming up!"
-echo ""
+	if ! test -f $SSH_CONFIG_FILE; then
+		create_ssh_config
+	fi
 
-ssh-keygen -t rsa -f $SSH_FILE -q -b 4096 -C "$GIT_EMAIL"
-cat $SSH_PUBLIC
-pbcopy < $SSH_PUBLIC
+	ensure_ssh_agent
 
-echo ""
-echo "Copied and ready to go!"
-echo "Head to https://github.com/settings/keys and add it!"
+	if ! command -v zsh &> /dev/null; then
+		install_zsh
+	fi
 
-echo ""
-echo "New SSH config"
-echo ""
-echo $SSH_CONFIG
+	echo "We're all done here!"
+}
 
-cat > SSH_CONFIG <<EOF
+# Stolen from https://stackoverflow.com/a/48509425
+function ensure_ssh_agent() {
+	# Ensure agent is running
+	ssh-add -l &>/dev/null
+
+	if [ "$?" == 2 ]; then
+		# Could not open a connection to your authentication agent.
+
+		# Load stored agent connection info.
+		test -r ~/.ssh-agent && \
+			eval "$(<~/.ssh-agent)" >/dev/null
+
+		ssh-add -l &>/dev/null
+		if [ "$?" == 2 ]; then
+			# Start agent and store agent connection info.
+			(umask 066; ssh-agent > ~/.ssh-agent)
+			eval "$(<~/.ssh-agent)" >/dev/null
+		fi
+	fi
+
+	# Load identities
+	ssh-add -l &>/dev/null
+	if [ "$?" == 1 ]; then
+		echo "Loading identities to ssh agent"
+		ssh-add -K $SSH_FILE
+	fi
+}
+
+function setup_git() {
+	# GIT
+	git config --global color.ui true
+	echo "git colors enabled"
+
+	git config --global user.email $GIT_EMAIL
+	echo "git email $GIT_EMAIL"
+
+	git config --global user.name $GIT_NAME
+	echo "git name $GIT_NAME"
+}
+
+function setup_ssh() {
+	echo "Creating new SSH key"
+	ssh-keygen -t rsa -f $SSH_FILE -q -b 4096 -C "$GIT_EMAIL"
+	cat $SSH_PUBLIC
+	pbcopy < $SSH_PUBLIC
+
+	echo "Copied and ready to go!"
+	echo "Head to https://github.com/settings/keys and add it!"
+}
+
+function create_ssh_config() {
+	echo "New SSH config"
+	echo $SSH_CONFIG_FILE
+
+cat > $SSH_CONFIG_FILE <<EOF
 Host *
  AddKeysToAgent yes
  UseKeychain yes
  IdentityFile $SSH_FILE
 EOF
+}
 
+function create_symlinks() {
+	echo "Creating sym links from your dotfiles"
+	for file in ${SYM_FILES[*]}; do
+		DOT=~/.dotfiles/$file
+		SYM=~/$file
+		echo "$DOT	-> 	$SYM"
+		ln -sfn $DOT $SYM
+	done
+}
 
-echo "Firing up a SSH Agent..."
-echo ""
-eval "$(ssh-agent -s)"
-ssh-add -K $SSH_FILE
+function install_brew() {
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
 
-# Dotfiles
-echo "Creating sym links from your dotfiles"
-echo ""
-echo ""
-for file in ${SYM_FILES[*]}
-do
-	DOT=~/.dotfiles/$file
-	SYM=~/$file
-	echo "$DOT	<- 	$SYM"
-	ln -s $DOT $SYM
-done
+function install_zsh() {
+	echo "Setting up ZSH!"
+	sudo sh -c "echo $(which zsh) >> /etc/shells" && chsh -s $(which zsh)
+}
 
-echo ""
-echo ""
-echo "Setting up ZSH!"
-sudo sh -c "echo $(which zsh) >> /etc/shells" && chsh -s $(which zsh)
+function install_programs() {
+	brew tap Homebrew/bundle
+	brew install mas
+	brew bundle
 
-echo ""
-echo ""
-echo "We're all done here!"
+	yarn global add xbuild fkill-cli
+}
 
-cd ~/
+main "$@"
 exit
